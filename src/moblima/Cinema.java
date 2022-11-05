1fa635a3184286ccf1b68f00e3d4291b645c6c9d
jklye <lyejinkai@gmail.com>
2022-11-05 14:00:45 +0800

    Changed cinemaCode aattribute from Integer to String and added getter functions

--- src/moblima/Cinema.java
+++ src/moblima/Cinema.java
@@ -4,22 +4,31 @@ import java.util.ArrayList;
 
 public class Cinema {
 
-    private static int currentCode=100;
-    private String cinemaCode;
+    //private static int currentCode=100;
     private String classLevel; // enum
+    private String location;
+    private String cinemaCode;
 
     public Cinema(String classLevel,
-                  String location) {
-        this.cinemaCode = currentCode++;
+                  String location,
+                  String cinemaCode) {
         this.classLevel = classLevel;
+        this.location = location;
+        this.cinemaCode = cinemaCode;
+    }
+
+    public String getClassLevel() {
+        return this.classLevel;
+    }
+
+    public String getLocation() {
+        return this.location;
     }
 
     public String getCinemaCode() {
         return this.cinemaCode;
     }
 
-    public String getClassLevel() {
-        return this.classLevel;
-    }
+    
     
 }
