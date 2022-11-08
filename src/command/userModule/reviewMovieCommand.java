1c55dd867078276fab4a99c2079e8cef10c19659
kiatCodes <studyxmk@gmail.com>
2022-11-08 17:07:34 +0800

    Command updated V4

--- src/command/userModule/reviewMovieCommand.java
+++ src/command/userModule/reviewMovieCommand.java
@@ -1,35 +1,39 @@
 package command.userModule;
 import command.Command;
+import moblima.Booking;
 import moblima.Movie;
 import moblima.MovieTicket;
 import moblima.Review;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 
 public class reviewMovieCommand implements Command{
     private int ticketID;
-    private ArrayList<MovieTicket> arrayTicket;
+    private ArrayList<Booking> arrayBooking;
     private int rating;
     private String reviewDesc;
     private ArrayList<Movie> arrayMovie;
     private int movieID;
 
-    public reviewMovieCommand(int ticketID, ArrayList<MovieTicket> arrayTicket, int rating, String reviewDesc, ArrayList<Movie> arrayMovie, int movieID){
+    public reviewMovieCommand(int ticketID, ArrayList<Booking> arrayBooking, int rating, String reviewDesc, ArrayList<Movie> arrayMovie, int movieID){
         this.arrayMovie=arrayMovie;
-        this.arrayTicket=arrayTicket;
+        this.arrayBooking=arrayBooking;
         this.movieID=movieID;
         this.ticketID=ticketID;
         this.reviewDesc=reviewDesc;
         this.rating=rating;
     }
     public void execute(){
-        for (MovieTicket ticket: arrayTicket){
-            if (ticket.getTicketID() == ticketID){
-                Review review = new Review(rating, reviewDesc);
-                for (Movie movie: arrayMovie){
-                    if (movie.getMovieId()==movieID){
-                        movie.getReviews().add(review);
-                        System.out.println("Review Created...");
+        for (Booking booking: arrayBooking){
+            for (MovieTicket ticket: booking.getTickets()){
+                if (ticket.getTicketID() == ticketID){
+                    Review review = new Review(rating, reviewDesc);
+                    for (Movie movie: arrayMovie){
+                        if (movie.getMovieId()==movieID){
+                            movie.getReviews().add(review);
+                            System.out.println("Review Created...");
+                        }
                     }
                 }
             }
@@ -37,3 +41,4 @@ public class reviewMovieCommand implements Command{
         System.out.println("ticket ID cannot be found...");
     }
 }
+
