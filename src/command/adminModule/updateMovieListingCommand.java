25896f165bfe5ee90d206fe715dd270d330547c3
kiatCodes <studyxmk@gmail.com>
2022-11-08 16:14:54 +0800

    UpdateShow and UpdateMovie

--- src/command/adminModule/updateMovieListingCommand.java
+++ src/command/adminModule/updateMovieListingCommand.java
@@ -1,8 +1,29 @@
 package command.adminModule;
 import command.Command;
+import moblima.Movie;
+
+import java.util.ArrayList;
 
 public class updateMovieListingCommand implements Command {
+    private ArrayList<Movie> movieArray;
+    private int movieID;
+    private String status;
+    updateMovieListingCommand(ArrayList<Movie> movieArray, int movieID, String status){
+        this.movieArray=movieArray;
+        this.movieID=movieID;
+        this.status=status;
+    }
     public void execute(){
+        Movie movieToBeUpdated;
+        for (Movie movie: movieArray){
+            if (movie.getMovieId() == movieID){
+                movieToBeUpdated = movie;
+                break;
+            }
+            else System.out.println("Movie not found...upda");
+        }
 
+        movieToBeUpdated.setStatus(status);
+        System.out.println("Movie successfully updated...");
     }
 }
--- src/command/adminModule/updateShowCommand.java
+++ src/command/adminModule/updateShowCommand.java
@@ -1,7 +1,29 @@
 package command.adminModule;
 import command.Command;
-public class updateShowCommand implements Command{
-    public void execute(){
+import moblima.Show;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class updateShowCommand implements Command{
+    private ArrayList<Show> showArray;
+    private int showID;
+    private LocalDateTime newDateTime;
+    public updateShowCommand(ArrayList<Show> showArray, int showID, LocalDateTime newDateTime){
+        this.newDateTime=newDateTime;
+        this.showArray=showArray;
+        this.showID=showID;
+    }
+    public void execute(){
+        Show showToBeUpdated;
+        for (Show show: showArray){
+            if (show.getShowId() == showID){
+                showToBeUpdated = show;
+                break;
+            }
+            else System.out.println("Show not found...");;
+        }
+        showToBeUpdated.setShowTime(newDateTime);
+        System.out.println("Show successfully updated...");
     }
 }
