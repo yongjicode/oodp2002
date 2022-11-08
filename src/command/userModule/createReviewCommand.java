5c9df1fac4f232fbd55618cab8a658d7047f18ee
kiatCodes <studyxmk@gmail.com>
2022-11-08 16:20:55 +0800

    Added CreateReviewCommand

---
+++ src/command/userModule/createReviewCommand.java
@@ -0,0 +1,41 @@
+package command.userModule;
+import account.*;
+
+import command.Command;
+import moblima.*;
+import account.Account;
+
+import java.util.ArrayList;
+
+public class createReviewCommand {
+
+    private int ticketID;
+    private ArrayList<MovieTicket> arrayTicket;
+    private int rating;
+    private String reviewDesc;
+    private ArrayList<Movie> arrayMovie;
+    private int movieID;
+
+    public createReviewCommand(int ticketID, ArrayList<MovieTicket> arrayTicket, int rating, String reviewDesc, ArrayList<Movie> arrayMovie, int movieID){
+        this.arrayMovie=arrayMovie;
+        this.arrayTicket=arrayTicket;
+        this.movieID=movieID;
+        this.ticketID=ticketID;
+        this.reviewDesc=reviewDesc;
+        this.rating=rating;
+    }
+    public void execute(){
+        for (MovieTicket ticket: arrayTicket){
+            if (ticket.getTicketID() == ticketID){
+                Review review = new Review(rating, reviewDesc);
+                for (Movie movie: arrayMovie){
+                    if (movie.getMovieId()==movieID){
+                        movie.getReviews().add(review);
+                        System.out.println("Review Created...");
+                    }
+                }
+            }
+        }
+        System.out.println("ticket ID cannot be found...");
+    }
+}
