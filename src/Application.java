a1735928bdb9bfeb0876f5c69d2261635ebdac5a
jklye <lyejinkai@gmail.com>
2022-11-11 22:00:10 +0800

    change from StandardCharsets.ACSII to StandardCharsets.UTF_16 for reading CSV

--- src/Application.java
+++ src/Application.java
@@ -5,11 +5,18 @@ import moblima.SilverVillage;
 import moblima.cineplex.Cineplex;
 import moblima.cineplex.cinema.Cinema;
 import moblima.cineplex.cinema.CinemaClass;
+import moblima.cineplex.CineplexList;
 import moblima.movie.Movie;
 import moblima.movie.MovieStatus;
+import moblima.movie.MovieList;
+import moblima.movie.review.ReviewList;
+import moblima.booking.Booking;
+import moblima.booking.BookingHistory;
 import moblima.show.Show;
+import moblima.show.ShowList;
 import system.PublicHoliday;
 import system.SystemSettings;
+import moblima.CSVReader;
 
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -28,31 +35,6 @@ public class Application {
 
 	public void run() {
 
-//		ShowList showList = CSVReader.readShowsFromCSV("src/database/showDB.csv");
-//		ArrayList<Movie> arrayMovie = CSVReader.readMoviesFromCSV("src/database/movieDB.csv");
-//		MovieList movieList = new MovieList();
-//		for (Movie movie: arrayMovie){
-//			movieList.addMovie(movie);
-//		}
-//
-//		ArrayList<Booking> arrayBooking = CSVReader.readBookingsFromCSV("src/database/bookingDB.csv");
-//		BookingHistory bookingHistory = new BookingHistory();
-//		for (Booking booking: arrayBooking){
-//			bookingHistory.addBooking(booking);
-//		}
-//
-//		ArrayList<Cineplex> arrayCineplex = CSVReader.readCineplexFromCSV("src/database/companyDB.csv", showList.getShows());
-//		CineplexList cineplexList = new CineplexList();
-//		for (Cineplex cineplex: arrayCineplex){
-//			cineplexList.addCineplex(cineplex);
-//		}
-//
-//		ArrayList<ReviewList> arrayReview = CSVReader.readReviewsFromCSV("src/database/reviewListDB.csv");
-//
-//
-//		ArrayList<Account> arrayAccount = CSVReader.readAccountsFromCSV("src/database/accountDB.csv", arrayCineplex);
-
-
 		SystemSettings ss = new SystemSettings();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 		LocalDateTime dateTime = LocalDateTime.parse("2022-12-25 00:00", formatter);
@@ -66,45 +48,70 @@ public class Application {
 		//}
 
 
-		Movie a = new Movie("Lion King", MovieStatus.PREVIEW, "c", "d", Arrays.asList("guy1", "guy2", "woman1"), LocalDateTime.parse("2022-12-29 12:00", formatter));
-		Movie b = new Movie("Toy Story", MovieStatus.NOW_SHOWING, "x", "w", Arrays.asList("guy1", "woman2", "woman3"), LocalDateTime.parse("2022-12-26 12:00", formatter));
-		SilverVillage.getMovieList().addMovie(a);
-		SilverVillage.getMovieList().addMovie(b);
-		Cineplex tempCine = new Cineplex("hello cinema", "bedok");
-		Cineplex changiCine = new Cineplex("Golden Village", "changi");
-		Cinema cn = new Cinema(CinemaClass.GOLD);
-		Cinema cn2 = new Cinema(CinemaClass.NORMAL);
-		tempCine.addCinema(cn);
-		tempCine.addCinema(cn2);
-		Cinema cn3 = new Cinema(CinemaClass.PLATINUM);
-		Cinema cn4 = new Cinema(CinemaClass.GOLD);
-		changiCine.addCinema(cn3);
-		changiCine.addCinema(cn4);
-		tempCine.getShowList().addShow(new Show(LocalDateTime.of(2015,
-				Month.JULY, 29, 19, 30), cn, a));
-		tempCine.getShowList().addShow(new Show(LocalDateTime.of(2017,
-				Month.JULY, 29, 16, 20), cn2, a));
-		tempCine.getShowList().addShow(new Show(LocalDateTime.of(2018,
-				Month.JULY, 29, 16, 20), cn2, b));
-		tempCine.getShowList().addShow(new Show(LocalDateTime.of(2022,
-				Month.JULY, 29, 16, 20), cn, b));
-		changiCine.getShowList().addShow(new Show(LocalDateTime.of(2026,
-				Month.JULY, 29, 16, 20), cn3, b));
-		changiCine.getShowList().addShow(new Show(LocalDateTime.of(2026,
-				Month.JULY, 29, 16, 20), cn4, a));
+		// Movie a = new Movie("Lion King", MovieStatus.PREVIEW, "c", "d", Arrays.asList("guy1", "guy2", "woman1"), LocalDateTime.parse("2022-12-29 12:00", formatter));
+		// Movie b = new Movie("Toy Story", MovieStatus.NOW_SHOWING, "x", "w", Arrays.asList("guy1", "woman2", "woman3"), LocalDateTime.parse("2022-12-26 12:00", formatter));
+		// SilverVillage.getMovieList().addMovie(a);
+		// SilverVillage.getMovieList().addMovie(b);
+		// Cineplex tempCine = new Cineplex("hello cinema", "bedok");
+		// Cineplex changiCine = new Cineplex("Golden Village", "changi");
+		// Cinema cn = new Cinema(CinemaClass.GOLD);
+		// Cinema cn2 = new Cinema(CinemaClass.NORMAL);
+		// tempCine.addCinema(cn);
+		// tempCine.addCinema(cn2);
+		// Cinema cn3 = new Cinema(CinemaClass.PLATINUM);
+		// Cinema cn4 = new Cinema(CinemaClass.GOLD);
+		// changiCine.addCinema(cn3);
+		// changiCine.addCinema(cn4);
+		// tempCine.getShowList().addShow(new Show(LocalDateTime.of(2015,
+		// 		Month.JULY, 29, 19, 30), cn, a));
+		// tempCine.getShowList().addShow(new Show(LocalDateTime.of(2017,
+		// 		Month.JULY, 29, 16, 20), cn2, a));
+		// tempCine.getShowList().addShow(new Show(LocalDateTime.of(2018,
+		// 		Month.JULY, 29, 16, 20), cn2, b));
+		// tempCine.getShowList().addShow(new Show(LocalDateTime.of(2022,
+		// 		Month.JULY, 29, 16, 20), cn, b));
+		// changiCine.getShowList().addShow(new Show(LocalDateTime.of(2026,
+		// 		Month.JULY, 29, 16, 20), cn3, b));
+		// changiCine.getShowList().addShow(new Show(LocalDateTime.of(2026,
+		// 		Month.JULY, 29, 16, 20), cn4, a));
 
-		SilverVillage.getCineplexList().addCineplex(tempCine);
-		SilverVillage.getCineplexList().addCineplex(changiCine);
-		Account[] accounts = new Account[4];
-		accounts[0] = new UserAccount("apple", "sauce", Privilege.User, "123@gmail.com", "999", "peter");
-		accounts[1] = new CineplexAdminAccount("orange", "sauce", Privilege.CineplexAdmin, tempCine, "abc@gmai.com", "992", "stacey");
-		accounts[2] = new CineplexAdminAccount("banana", "sauce", Privilege.CineplexAdmin, changiCine, "999@gmai.com", "992", "harold");
-		accounts[3] = new CompanyAdminAccount("durian", "sauce", Privilege.CompanyAdmin, "999@gmai.com", "992", "june");
-		SystemSettings.addAccount(accounts[0]);
-		SystemSettings.addAccount(accounts[1]);
-		SystemSettings.addAccount(accounts[2]);
-		SystemSettings.addAccount(accounts[3]);
+		// SilverVillage.getCineplexList().addCineplex(tempCine);
+		// SilverVillage.getCineplexList().addCineplex(changiCine);
+		// Account[] accounts = new Account[4];
+		// accounts[0] = new UserAccount("apple", "sauce", Privilege.User, "123@gmail.com", "999", "peter");
+		// accounts[1] = new CineplexAdminAccount("orange", "sauce", Privilege.CineplexAdmin, tempCine, "abc@gmai.com", "992", "stacey");
+		// accounts[2] = new CineplexAdminAccount("banana", "sauce", Privilege.CineplexAdmin, changiCine, "999@gmai.com", "992", "harold");
+		// accounts[3] = new CompanyAdminAccount("durian", "sauce", Privilege.CompanyAdmin, "999@gmai.com", "992", "june");
+		// SystemSettings.addAccount(accounts[0]);
+		// SystemSettings.addAccount(accounts[1]);
+		// SystemSettings.addAccount(accounts[2]);
+		// SystemSettings.addAccount(accounts[3]);
+
 		// end of test cases
+
+		ShowList showList = CSVReader.readShowsFromCSV("src/database/showDB.csv");
+		ArrayList<Movie> arrayMovie = CSVReader.readMoviesFromCSV("src/database/movieDB.csv");
+		MovieList movieList = new MovieList();
+		for (Movie movie: arrayMovie){
+			movieList.addMovie(movie);
+		}
+
+		ArrayList<Booking> arrayBooking = CSVReader.readBookingsFromCSV("src/database/bookingDB.csv");
+		BookingHistory bookingHistory = new BookingHistory();
+		for (Booking booking: arrayBooking){
+			bookingHistory.addBooking(booking);
+		}
+
+		ArrayList<Cineplex> arrayCineplex = CSVReader.readCineplexFromCSV("src/database/companyDB.csv", showList.getShows());
+		CineplexList cineplexList = new CineplexList();
+		for (Cineplex cineplex: arrayCineplex){
+			cineplexList.addCineplex(cineplex);
+		}
+
+		ArrayList<ReviewList> arrayReview = CSVReader.readReviewsFromCSV("src/database/reviewListDB.csv");
+
+
+		ArrayList<Account> arrayAccount = CSVReader.readAccountsFromCSV("src/database/accountDB.csv", arrayCineplex);
 
 		// load in CSV
 
